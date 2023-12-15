a6d3e9d57f36913508b2a2b741ccd967b78f7f2f
viveknhc <viveknhc@gmail.com>
2023-12-15 11:03:08 +0530

    16/12/23

---
+++ assets/javascript/script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".service-slider", {
+    slidesPerView: 3.5,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: "#nextButton",
+      prevEl: "#prevButton",
+    },
+  });
