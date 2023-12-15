ed838e4d387f95e49f2b3251e2cb232266680ab5
viveknhc <viveknhc@gmail.com>
2023-12-16 03:28:35 +0530

    dksf

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,23 +1,53 @@
+// Loader
+setTimeout(function () {
+  $('.loader-box').fadeToggle();
+}, 2000);
+
+
 var swiper = new Swiper(".service-slider", {
-    slidesPerView: 3.5,
-    spaceBetween: 30,
-    navigation: {
-      nextEl: "#nextButton",
-      prevEl: "#prevButton",
+  loop: true,
+  navigation: {
+    nextEl: "#nextButton",
+    prevEl: "#prevButton",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 1.5,
+      spaceBetween: 20
     },
-  });
-
-
-  // TAKING LAST IMAGE OF USER
-
-    function updateImages() {
-      const container = document.getElementById('userImgBox');
-      const allImages = container.getElementsByTagName('img');
-      const imageArray = Array.from(allImages);
-      container.innerHTML = '';
-      const startIndex = Math.max(0, imageArray.length - 3);
-      for (let i = startIndex; i < imageArray.length; i++) {
-          container.appendChild(imageArray[i].cloneNode(true));
-      }
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 30
+    },
+    1024: {
+      slidesPerView: 3.5,
+      spaceBetween: 40
+    }
   }
-  updateImages();
+
+});
+
+
+// TAKING LAST IMAGE OF USER
+
+function updateImages() {
+  const container = document.getElementById('userImgBox');
+  const allImages = container.getElementsByTagName('img');
+  const imageArray = Array.from(allImages);
+  container.innerHTML = '';
+  const startIndex = Math.max(0, imageArray.length - 3);
+  for (let i = startIndex; i < imageArray.length; i++) {
+    container.appendChild(imageArray[i].cloneNode(true));
+  }
+}
+updateImages();
+
+
+// lazy load
+
+$("img.lazy").lazyload({effect: "slideDown"}); 
+
+// aos
+
+AOS.init();
