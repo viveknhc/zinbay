21f5c34bacb3bcc2ba4a2d5ed8eb51c4923a4e34
viveknhc <viveknhc@gmail.com>
2023-12-15 16:10:58 +0530

    15/12/23

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,3 +6,18 @@ var swiper = new Swiper(".service-slider", {
       prevEl: "#prevButton",
     },
   });
+
+
+  // TAKING LAST IMAGE OF USER
+
+    function updateImages() {
+      const container = document.getElementById('userImgBox');
+      const allImages = container.getElementsByTagName('img');
+      const imageArray = Array.from(allImages);
+      container.innerHTML = '';
+      const startIndex = Math.max(0, imageArray.length - 3);
+      for (let i = startIndex; i < imageArray.length; i++) {
+          container.appendChild(imageArray[i].cloneNode(true));
+      }
+  }
+  updateImages();
